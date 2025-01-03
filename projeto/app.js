a75d0f62712e711adf04805f9fbc460094d7eedb
Lucas <lucasemanuelndq@gmail.com>
2025-01-03 02:02:26 -0300

    projeto finalizado

--- projeto/app.js
+++ projeto/app.js
@@ -18,7 +18,6 @@ function add_task() {
     let task_desc = desc_txt.value
     let verify = false
 
-
     for (i in tasks_array){
         if (tasks_array[i].name == task_title) {
             verify = true
@@ -39,12 +38,20 @@ function show_task() {
     for (var i = 0; i < tasks_array.length;i++){
         tasks.innerHTML += `
     <div class="card" id="${tasks_array[i].name}">
-        <h1>${tasks_array[i].name}</h1><a href="#" id="${i}" onclick="show_hide_desc(${i})">mostrar descrição</a>
+        <div id="card_info">
+        <p><h1>${tasks_array[i].name}</h1>
+        <a href="#" id="${i}" onclick="show_hide_desc(${i})">mostrar descrição</a><input type="checkbox" id="check_task${i} value="${i}" "><input type="button" value="delete" class="delete_button" onclick ="delete_task(${i})"></p>
+        </div>
         <p id="p_desc${i}" class="desc">${tasks_array[i].desc}</p> 
     </div>
     `
     }
     
+}
+
+function delete_task(id){
+    tasks_array.splice(id,1)
+    show_task()
 }
 
 function show_hide_desc(id) {
@@ -56,6 +63,34 @@ function show_hide_desc(id) {
         }
 
 }
+
+/*
+function delete_complete_task() {
+    let checkboxes = document.querySelectorAll('input[type="checkbox"]')
+    for (let i in checkboxes) {
+        let checkbox = checkboxes[i]
+        if (checkbox.checked){
+            delete_task(i)
+        }
+    }
+}
+*/
+function delete_complete_task() {
+    let checkboxes = document.querySelectorAll('input[type="checkbox"]')
+    for (let i in checkboxes) {
+        let checkbox = checkboxes[i]
+        if (checkbox.checked){
+            tasks_array[i].complete = true
+        }
+    }
+
+    for (let task = tasks_array.length-1; task >= 0; task--) {
+        if (tasks_array[task].complete){
+            delete_task(task)
+       }
+    }
+}
+
 
 function Task(name, desc, complete ) {
     this.name = name,
